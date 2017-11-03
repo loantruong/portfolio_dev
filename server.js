94bc017e8271e718e70424b799c1ca4fde6d252b
loan <loancrea@gmail.com>
2017-11-03 15:49:03 +0100

    test pug

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const nodemailer = require('nodemailer');
+const pug = require('pug');
 
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
